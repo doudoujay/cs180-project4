56d90e5f1290423931d38f3bc70936583fabc86a
doudoujay <doudoujay@qq.com>
2017-03-30 17:22:10 -0400

    finished FileUtiles

--- src/FileUtils.java
+++ src/FileUtils.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.List;
 
 /**
@@ -6,18 +7,80 @@ import java.util.List;
 public class FileUtils {
     public FileUtils() {
     }
+
+    public void writeObj(Object obj,
+                         String filePath) throws IOException {
+        File f = new File(filePath);
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+
+
+        fos = new FileOutputStream(f);
+        oos = new ObjectOutputStream(fos);
+        oos.writeObject(obj);
+        fos.close();
+        oos.close();
+
+
+    }
+
     public boolean saveWordTable(List<Word> wordTable,
-                                 String filePath){
+                                 String filePath) {
+        try {
+            writeObj(wordTable, filePath);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
 
     }
+
     public boolean savePageTable(List<Page> pageTable,
-                                 String filePath){
+                                 String filePath) {
+        try {
+            writeObj(pageTable, filePath);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
 
     }
-    public List<Word> getWordList(String filePath){
+
+    public List<Word> getWordList(String filePath) {
+        try {
+            FileInputStream fis = new FileInputStream(new File(filePath));
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            List<Word> result = (List<Word>) ois.readObject();
+            ois.close();
+            fis.close();
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+
 
     }
-    public List<Page> getPageList(String filePath){
-        
+
+    public List<Page> getPageList(String filePath) {
+        try {
+            FileInputStream fis = new FileInputStream(new File(filePath));
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            List<Page> result = (List<Page>) ois.readObject();
+            ois.close();
+            fis.close();
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
