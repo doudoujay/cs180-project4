f29778eabccdd2f3b7fb06db5729a74975a13f32
doudoujay <doudoujay@qq.com>
2017-03-30 09:51:42 -0400

    init FileUtiles

--- src/FileUtils.java
+++ src/FileUtils.java
@@ -1,5 +1,23 @@
+import java.util.List;
+
 /**
  * Created by jay on 3/30/17.
  */
 public class FileUtils {
+    public FileUtils() {
+    }
+    public boolean saveWordTable(List<Word> wordTable,
+                                 String filePath){
+
+    }
+    public boolean savePageTable(List<Page> pageTable,
+                                 String filePath){
+
+    }
+    public List<Word> getWordList(String filePath){
+
+    }
+    public List<Page> getPageList(String filePath){
+        
+    }
 }
---
+++ src/SearchThread.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jay on 3/30/17.
+ */
+public class SearchThread implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
