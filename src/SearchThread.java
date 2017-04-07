00004dd833def4b44b4acd147a9b6838efeb4c22
jay <doudoujay@qq.com>
2017-04-06 23:55:43 -0400

    init SearchThread

--- src/SearchThread.java
+++ src/SearchThread.java
@@ -1,10 +1,27 @@
 /**
  * Created by jay on 3/30/17.
  */
-public class SearchThread implements Runnable{
+public class SearchThread implements Runnable {
+    public int start;
+    public int finish;
+    public String[] terms;
+
+    public SearchThread(int start,
+                        int finish,
+                        String[] terms) {
+        this.start = start;
+        this.finish = finish;
+        this.terms = terms;
+    }
 
     @Override
     public void run() {
+//        TODO
 
+    }
+    public Word findTerm(String term){
+//        TODO
+//        Find and return the associated Word object for a given term
+        return null;
     }
 }
