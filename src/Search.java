c75c6f0c1f94533f071f63cb7b6da54ca401bb73
hsu82 <hsu82@purdue.edu>
2017-04-07 21:36:45 -0400

    Search and SearchThread Commit 1

--- src/Search.java
+++ src/Search.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -14,24 +18,63 @@ public class Search {
                   String pageListFile) {
         this.wordListFile = wordListFile;
         this.pageListFile = pageListFile;
+        pageList = Collections.synchronizedList(new ArrayList<Page>());
+        wordList = Collections.synchronizedList(new ArrayList<Word>());
+        resultSet = Collections.synchronizedList(new ArrayList<Result>());
+
     }
 
     public void setup(String wordListFile,
                       String pageListFile) {
-//        TODO
+//
+        FileUtils fileUtil = new FileUtils();
+
+        pageList = fileUtil.getPageList(pageListFile);
+        wordList = fileUtil.getWordList(wordListFile);
+      //  fileUtil.savePageTable(pageList,pageListFile);
+     //   fileUtil.saveWordTable(wordList,wordListFile);
+
+
 
     }
 
     public List<Result> executeQuery(String query) {
 //        TODO
+        //Execute a query over built tables and return an arraylist of Result objects.
+
+        // Split query String into array of terms
+        String[] terms = query.split(" ");
+
+        //Create 5 Threads using SearchThread class
+        int start = 0;
+        int end = 200;
+        Thread[] threads ={ new SearchThread(0,200,terms), new SearchThread(201,400,terms)};//add more threads?
+
+        //Start Threads
+        for( Thread t : threads)
+            t.start();
+
+
+        for( Thread t: threads)
+            t.join();
+
+
+        //sort list by score
+        sort();
+
         return null;
     }
 
     public void nullCheck() {
 //        TODO
+        //method to check if we have not read from our lists from File
+
+
     }
 
     public void sort() {
 //        TODO
+        //sort ArrayList of Results by their score
+
     }
 }
